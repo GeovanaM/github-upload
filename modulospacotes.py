4e9daeb853d7e2482a78cbcab0b7e389fb7df5ff
GeovanaM <geeovanamartins@gmail.com>
2021-02-12 15:45:00 -0300

    initializing repository

---
+++ modulospacotes.py
@@ -0,0 +1,40 @@
+#Modulos
+
+#import statistics as est
+
+#from statistics import *
+
+#from statistics import mean, median
+
+'''z = [10, 20, 20, 40]
+x = mean(z)
+y = median(z)
+print(x)
+print(y)'''
+
+
+#Funções
+'''Criando funções'''
+
+'''def imprime(n):
+    print(n)
+imprime("texto")'''
+
+'''def potencia(n):
+    return n * n
+x = potencia(3)
+print(x)'''
+
+'''def intervalo(ini=1, fim=10):
+    for inic in range(1, fim+1):
+        print(inic)
+intervalo(2, 5)'''
+
+print(abs(-200))
+from statistics import *
+x =[10, 20, 30]
+print("Media = ", mean(x))
+
+from numpy import *
+a = random.random((8,8))
+print(a)
---
+++ testevariavel.py
@@ -0,0 +1,43 @@
+
+#print e input
+'''var = input("Digite o valor")
+var = int(var)
+print(type(var))'''
+
+#Decisão
+'''nota = 5
+if nota <= 4:
+    print("Aprovado")
+elif nota >= 4 and nota <=6:
+    print("exame")
+else:
+    print("Aprovado")'''
+
+#Repetição
+#While
+'''count = 1
+while count <=5:
+    print(count)
+    count += 1'''
+
+#For in range
+'''for n in range(0, 10):
+    if n == 4:
+        continue;
+    print(n)'''
+
+#Listas
+'''lst = [1,2,3,4,5]
+lst2 = [1,2,3,"4",True]
+lst3 = [12,[1,2,3]]
+lst4 = list(range(0,10))
+
+print(lst4)
+print(len(lst))
+lst4[0] = 3
+
+for n in range(0,len(lst4)):
+    print(lst4[n])'''
+
+
+
